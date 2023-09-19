d91bd6be2a92f5952650669fa9da38b893f78a25
juanjosebl2 <juanjosebl2@gmail.com>
2023-09-19 10:50:05 +0200

    coomit

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
         <AppLayout
             header={
                 <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                    Dashboard
+                    Dashboar
                 </h2>
             }>
 
